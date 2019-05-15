726fa2f47841565658f2b4f6a24c72ee9aefd2fb
Silvio Echsle <s.echsle@web.de>
2019-05-15 13:22:22 +0200

    Added new select_biggest function and merged random with normal image_load

--- readFileExample/machine learning grundlagen.py
+++ readFileExample/machine learning grundlagen.py
@@ -14,47 +14,38 @@ import matplotlib as mpl
 from sklearn.model_selection import train_test_split
 from functools import reduce
 
-"""
-divide_in_train_test_data divides given images into train and test data.
-The y values are the indices of the np.array in the given list.
-The given data will be evenly represented in the train and test data (by percentage).
+def divide_in_train_test_data(data: [(str, np.ndarray)], testsize: float = 0.8)->(np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    """
+    divide_in_train_test_data divides given images into train and test data.
+    The y values are the indices of the np.array in the given list.
+    The given data will be evenly represented in the train and test data (by percentage).
 
-Arguments:
-    data: data is a list containing tuples of name of the class and the given data (for example images) flattend
-          so that one row represents one element of the data to learn from.
+    Arguments:
+        data: data is a list containing tuples of name of the class and the given data (for example images) flattend
+              so that one row represents one element of the data to learn from.
     
-    testsize: testsize determines the procentual size of data used for testing afterwards.
+        testsize: testsize determines the procentual size of data used for testing afterwards.
     
     Return:
         X_train: list of data to train (each image as numpy.ndarray)
         X_test:list of data to test (each image as numpy.ndarray)
         y_train: list of data to train (each image as numpy.ndarray)
         y_test: list of data to test (each image as numpy.ndarray)
-        
-"""
-
-def divide_in_train_test_data(data: [(str, np.ndarray)], testsize: float = 0.8)->(np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    """
     #calculating testsize from a given number (int or float) into a float in]0,1]
     if (type(testsize) == int or testsize > 1.0):
         testsize = testsize / float(10**(math.ceil(math.log10(testsize))))
     
-    X_train, X_test, y_train, y_test = [],[],[],[]
-    mylists = [X_train, X_test, y_train, y_test]
-    
-    #splitting every class data into X_train, X_test, y_train, y_test and ad it up in the lists
-    for i in range(len(data)):
-        for x, lst in zip(train_test_split(data[i][1], np.repeat(i, data[i][1].shape[0]),test_size = testsize), mylists):
-            lst.append(x)
-            
-    #concatenate the test and train data sets into one np.array
-    for i in range(len(mylists)):
-        mylists.append(np.concatenate(mylists[0], axis = 0))
-        mylists.pop(0)
-    
-    #returning the list as a tuple
-    return tuple(mylists)
+    #constructing y as indices of the class name in "data"
+    #concatenating the matrices to one big 
+    #and dividing it
+    y = np.concatenate([np.repeat(i, data[i][1].shape[0]) for i in range(len(data))])
+    x = np.concatenate([data[i][1] for i in range(len(data))], axis = 0)
+    X_train, X_test, y_train, y_test = train_test_split(x,y, stratify = y )
+
+    return X_train, X_test, y_train, y_test
         
-def load_images(path: str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64) -> (np.ndarray):
+def load_images(path: str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64, random: bool = False) -> (np.ndarray):
     """
     Load a percentage of all images in path with matplotlib that have given file_ending
 
@@ -74,6 +65,11 @@ def load_images(path: str, file_ending: str=".jpg", percentage: float = 1.0, typ
     
     images = []
     data = list(filter(lambda x: x.endswith(file_ending), os.listdir(path))) #all data with given file_ending found in the file
+    
+    #randomising if random is set
+    if(random):
+        data = random.shuffle(data)
+    
     count = math.ceil(len(data)*percentage) #number of images wanted
 
     #add up of the images
@@ -82,11 +78,13 @@ def load_images(path: str, file_ending: str=".jpg", percentage: float = 1.0, typ
     print( path.split("/")[-1] + " enthält ",len(images) , " Bilder.")
     return np.asarray(images)
 
-def load_image_folder(path:str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64) -> [(str, np.ndarray)]:
+def load_image_folder(path:str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64, random: bool = False) -> [(str, np.ndarray)]:
     """
     Loads a percentage of all images of every subfolder (non recursiv) with matplotlib that have given file_ending
 
     Arguments:
+    random: true if pictures should be choosen randomly    
+    
     path: path of directory of which contains folders containing image files that can be assumed to have all the same dimensions
 
     file_ending: string that image files have to end with, if not->ignore file
@@ -99,52 +97,9 @@ def load_image_folder(path:str, file_ending: str=".jpg", percentage: float = 1.0
     classes = []
     for root, dirs, files in os.walk(path):
         for name in dirs:
-            x = load_images(root + "/" + name, file_ending, percentage, typ)
+            x = load_images(root + "/" + name, file_ending, percentage, typ, random)
             classes.append((name, x))
     return classes
-
-def  select_random_images(path: str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64) -> (np.ndarray):
-    "see load_image but random"
-    if (type(percentage) == int or percentage > 1.0):
-        percentage = percentage / float(10**(math.ceil(math.log10(percentage))))
-    
-    images = []
-    data = list(filter(lambda x: x.endswith(file_ending), os.listdir(path))) #all data with given file_ending found in the file
-    count = math.ceil(len(data)*percentage) #number of images wanted
-    random.shuffle(data)
-
-    #add up of the images
-    for name in data[:count] :
-        images += [np.asarray(mpl.image.imread(path + "/" + name), dtype = typ).flatten()]
-    print( path.split("/")[-1] + " enthält ",len(images) , " Bilder.")
-    return np.asarray(images)
-    
-def random_images(path:str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64) -> (np.ndarray, [(str, int)]):
-    """
-    Loads a percentage of all images randomly choosen out of every subfolder (non recursiv) with matplotlib that have given file_ending.
-
-    Arguments:
-    path: path of directory of which contains folders containing image files that can be assumed to have all the same dimensions
-
-    file_ending: string that image files have to end with, if not->ignore file
-
-    percentage: percentage of images loaded from every subfolder.
-
-    typ: typ detemines the type the images will be loaded as.
-
-    Return:
-    data: randomly choosen but even spaced between the subfolders flattened images in a matrix (each image as numpy.ndarray and dtype decided by argument)
-    classes: list of tuple including the name of every subfolder with the number of pictures taken.
-    """
-
-    classes, data = [], []
-    for root, dirs, files in os.walk(path):
-        for name in dirs:
-            x = select_random_images(root + "/" + name, file_ending = file_ending, percentage = percentage, typ = typ)
-            classes.append((name, len(x)))
-            data.append(x)
-    data = np.concatenate(data, axis = 0)
-    return data, classes
 
 def random_sets(path:str, file_ending: str=".jpg", percentage: float = 1.0, typ: type = np.float64, count: int = 30) -> ([str], [np.ndarray], [np.ndarray]):
     """
@@ -196,7 +151,16 @@ def random_sets(path:str, file_ending: str=".jpg", percentage: float = 1.0, typ:
     references = map(lambda x : np.asarray(x), references)
     return classes, sets, references
     
-if __name__ == '__main__' :
-    randomclasses = random_sets("C:/Users/sechs/Downloads/EuroSAT/2750", file_ending = ".jpg", percentage = 0.02, typ = np.uint8)
+def select_biggest(classes: [(str, np.ndarray)], number: int) -> [(str, np.ndarray)]:
+    classes.sort(key = lambda x: x[1].shape[0])
+    return classes[:number]
 
+if __name__ == '__main__' :
+    path = "C:/Users/sechs/Downloads/EuroSAT/2750"
+    x = load_image_folder(path, file_ending = ".jpg", percentage = 0.02, typ = np.uint8)
+    x = select_biggest(x, 5)
+    print(len(x))
+    mylists = divide_in_train_test_data( x, testsize = 0.8)
+    for i in mylists:
+        print(i.shape)
     
