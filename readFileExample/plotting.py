2b1fed4238c0a28b39262a895deffde59f07be8d
Silvio Echsle <s.echsle@web.de>
2019-06-12 12:36:51 +0200

    Adding new features fixing bugs and changing the look of the plot

--- readFileExample/plotting.py
+++ readFileExample/plotting.py
@@ -5,42 +5,35 @@ Created on Wed May 22 13:07:41 2019
 @author: sechs
 """
 import matplotlib.pyplot as plt
+import random
+import numpy as np
 
-def plot(ax, x_values: [int], y_values: [int], methode: str ):
-    """point_list = []
-    dictionary = dict()
-    for x in point_list:
-        if dictionary.get((x.train_method_name, x.acc_idx_name) is None):
-            dictionary.update({(x.train_method_name, x.acc_idx_name) : [x]})
-        else:
-           dictionary.update({(x.train_method_name, x.acc_idx_name) : dictionary.get((x.train_method_name, x.acc_idx_name)).append(x)}) 
-    print("Hello, World")
-    for y in dictionary.values:
-        plt.plot([x.num_of_files for x in y])
-    """
-    ax.plot(x_values, y_values, "ro")
-    ax.set_title(methode)
+def plot(metrics: [str] = ["Cross" for i in range(3)],
+         x_values: [[int]] = [[[i+j+0, i+j+1, i+j+2, i+j+3] for i in range(5)] for j in range(3)],
+         y_values: [[int]] = [[list(range(i*4+j, (i+1)*4+j)) for i in range(5)] for j in range(3)],
+         mls: [str] = ["boosting" for i in range(5)]
+         ):
+    
+    fig, axs = plt.subplots(len(metrics), len(mls), figsize=(10, 5))
+    for z in range(len(mls)):
+        axs[0, z].set_title(mls[z])
+        for y in range(len(metrics)):
+            if y != fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[0] - 1:
+                axs[y, z].set_xticklabels([])
+            if z > 0 and z < fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1]:
+                axs[y, z].set_yticklabels([])
+            
+    for x in range(len(x_values)):
+        for i in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[0]):
+            for j in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1]):
+                axs[i, j].plot(x_values[x], y_values[x], ["b", "g", "r", "c", "m", "y", "k", "w"][x%8] + "o")
+                
+    plt.ylabel(r'Classification Metric Value', fontsize = 25, horizontalalignment='center', verticalalignment='center', y = 1.5, labelpad = 480.0)
+    plt.xlabel("Number of variables for training", fontsize = 25, horizontalalignment='center', x = -1.5)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.savefig('plot1.jpg')
+    plt.show()
     return
     
 if __name__ == "__main__":
-    
-    fig, axs = plt.subplots(3, 5, figsize=(8, 4.8))
-    x_values = [[0, 1, 2, 3] for i in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1])]
-    y_values = [list(range(i*4, (i+1)*4)) for i in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1])]
-    mls = ["boosting" for i in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1])]
-    print(fig.axes[0].get_subplotspec().get_gridspec().get_geometry())
-    for i in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[0]):
-        for j in range(fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[1]):
-            plot(axs[i, j], x_values, y_values, mls[j] if i == 0 else "")
-            if i != fig.axes[0].get_subplotspec().get_gridspec().get_geometry()[0] - 1:
-                axs[i, j].get_xaxis().set_visible(False)
-            if j != 0:
-                axs[i, j].get_yaxis().set_visible(False)
-                
-    plt.ylabel("Classification Metric Value", fontsize = 100)
-    plt.xlabel("Number of variables for training")
-    plt.subplots_adjust(wspace=0, hspace=0)  
-    #plt.tight_layout(pad=0.2, w_pad=0.2, h_pad=0.2)
-    plt.savefig('plot1.jpg')
-    plt.show()
-    
+    plot()
