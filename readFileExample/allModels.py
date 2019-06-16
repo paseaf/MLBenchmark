e0e5003a3fd1ed295a3ef26901fc006199b23707
Ziyang Li <ziyang.li@campus.tu-berlin.de>
2019-06-16 09:43:44 +0200

    change parameters to reduce time

--- readFileExample/allModels.py
+++ readFileExample/allModels.py
@@ -28,14 +28,15 @@ weight by distance
 """
 def modelKNN(train_set_x, train_set_y, n_neighbors=None):
     # optimize for optimal number neighbors or take nr of neighbors
-    if n_neighbors is None:
-        param_grid = {'n_neighbors': range(1,np.sqrt(train_set_y.shape[0]))}
-        grid = GridSearchCV(estimator=KNeighborsClassifier(weights='distance'), param_grid=param_grid, scoring='accuracy', cv=5, n_jobs=-1)
-        fit = grid.fit(train_set_x, train_set_y)
-        print(f"optimal hyper-parameters: {fit.best_params_}")
-    else:
-        knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights='distance')
-        fit = knn.fit(train_set_x, train_set_y)
+    # if n_neighbors is None:
+        # param_grid = {'n_neighbors': range(1, int(np.sqrt(train_set_y.shape[0])))}
+        # grid = GridSearchCV(estimator=KNeighborsClassifier(weights='distance'), param_grid=param_grid, scoring='accuracy', cv=5, n_jobs=-1)
+        # fit = grid.fit(train_set_x, train_set_y)
+        # print(f"optimal hyper-parameters: {fit.best_params_}")
+    # else:
+    n_neighbors = int(np.sqrt(train_set_y.shape[0]))    # commented out gridsearch for
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights='distance')
+    fit = knn.fit(train_set_x, train_set_y)
     return fit
 
 """
@@ -48,10 +49,16 @@ def modelForest(train_set_x, train_set_y, trees = 10):
     # use 'area under the curve' as optimization value
     #grid = GridSearchCV(estimator=RandomForestClassifier(n_estimators=trees), param_grid=param_grid, scoring='roc_auc', cv=5, n_jobs=-1)
     # use 'accuracy' as optimization value
-    grid = GridSearchCV(estimator=RandomForestClassifier(n_estimators=trees), param_grid=param_grid, scoring='accuracy', cv=5, n_jobs=-1)
-    fit = grid.fit(train_set_x, train_set_y)
-    print(f"optimal hyper-parameters: {fit.best_params_}")
-    return fit
+    # grid = GridSearchCV(estimator=RandomForestClassifier(n_estimators=trees), param_grid=param_grid, scoring='accuracy', cv=5, n_jobs=-1)
+    # fit = grid.fit(train_set_x, train_set_y)
+    # print(f"optimal hyper-parameters: {fit.best_params_}")
+
+    # fixed parameter
+    model = RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=2, bootstrap=True,
+                                   max_features='sqrt', oob_score=True)
+    model.fit(train_set_x, train_set_y)
+
+    return model
 
 """  
 use Radial Basis Function as kernel and then GridSearch to find optimal 'gamma' and 'C' parameters
@@ -72,10 +79,15 @@ scale?: scale between [0,1] or mean = 0 & var = 1
 """
 def modelMLP(train_set_x, train_set_y):
     #param_grid = {'solver': ['lbfgs','adam'], 'max_iter': [1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000 ], 'alpha': 10.0 ** -np.arange(1, 10), 'hidden_layer_sizes':np.arange(1, 10), 'random_state':[0,1,2,3,4,5,6,7,8,9]}
-    param_grid = {'solver': ['lbfgs','adam'], 'hidden_layer_sizes':np.arange(1, 10)}
-    model = GridSearchCV(MLPClassifier(), param_grid, cv=5, scoring='roc_auc', n_jobs=-1)
-    fit = model.fit(train_set_x, train_set_y)
-    return fit
+    # param_grid = {'solver': ['lbfgs','adam'], 'hidden_layer_sizes':np.arange(1, 10)}
+    # model = GridSearchCV(MLPClassifier(), param_grid, cv=5, scoring='roc_auc', n_jobs=-1)
+    # fit = model.fit(train_set_x, train_set_y)
+    # return fit
+    m, N = 1, train_set_y.shape[0]
+    model = MLPClassifier(hidden_layer_sizes=(int(2*np.sqrt((m+2)*N)), int(m*np.sqrt(N/(m+2)))))
+    model.fit(train_set_x, train_set_y)
+    return model
+
 
 """  
 TODO: implement ensemble method
@@ -95,12 +107,14 @@ min_samples_split: The minimum number of samples required to split an internal n
 min_samples_leaf: The minimum number of samples required to be at a leaf node (deafult=1)
 """
 def modelCT(train_set_x, train_set_y):
-    ctree = DecisionTreeClassifier(random_state=1)
-    param_grid = {'max_depth':range(1,50), 'min_samples_split':range(1,50)}
-    grid = GridSearchCV(ctree, param_grid, cv=5, scoring='roc_auc')
-    fit = grid.fit(train_set_x, train_set_y)
-    print(f"optimal hyper-parameters: {fit.best_params_}")
-    return fit
+    # ctree = DecisionTreeClassifier(random_state=1)
+    # param_grid = {'max_depth':range(1,50), 'min_samples_split':range(1,50)}
+    # grid = GridSearchCV(ctree, param_grid, cv=5, scoring='roc_auc')
+    # fit = grid.fit(train_set_x, train_set_y)
+    # print(f"optimal hyper-parameters: {fit.best_params_}")
+    # return fit
+    model = DecisionTreeClassifier().fit(train_set_x, train_set_y)
+    return model
 
 """  
 Boosting
@@ -116,10 +130,13 @@ Logistic Regression
 C: Inverse of regularization strength; must be a positive float. Like in support vector machines, smaller values specify stronger regularization.
 """
 def modelLR(train_set_x, train_set_y):
-    logreg = LogisticRegression()
-    param_grid = {"C":[0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}
-    kfold = KFold(n_splits=5, random_state=7)
-    grid = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=kfold)
-    fit = grid.fit(train_set_x, train_set_y)
-    print(f"optimal hyper-parameters: {fit.best_params_}")
-    return fit
+    # logreg = LogisticRegression()
+    # param_grid = {"C":[0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}
+    # kfold = KFold(n_splits=5, random_state=7)
+    # grid = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=kfold)
+    # fit = grid.fit(train_set_x, train_set_y)
+    # print(f"optimal hyper-parameters: {fit.best_params_}")
+    # return fit
+    model = LogisticRegression(multi_class='ovr', solver='saga', max_iter=200, n_jobs=-1)
+    model.fit(train_set_x, train_set_y)
+    return model
